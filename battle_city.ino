531ab3d67eb764503b62d1a32746f45c498c7dfb
Warboss-rus <Urgaksh@rambler.ru>
2019-04-08 22:36:26 +0300

    [battle_city] Now targets STM32 and 128*160 color screen. Added new types of terrain. Also sends meteo data via RF24

--- battle_city.ino
+++ battle_city.ino
@@ -1,18 +1,18 @@
 #define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+#define SCREEN_HEIGHT 160
 #define SPRITE_SIZE 8
 #define BULLET_SIZE 2
 #define TILE_ROWS 16 // SCREEN_WIDTH / SPRITE_SIZE
-#define TILE_COLUMNS 8 // SCREEN_HEIGHT / SPRITE_SIZE
+#define TILE_COLUMNS 20 // SCREEN_HEIGHT / SPRITE_SIZE
 #define TANK_STEP_DURATION 125
 #define BULLET_STEP_DURATION 50
 #define RELOAD_FRAMES 40
-#define JOYSTICK_X_PIN A0
-#define JOYSTICK_Y_PIN A1
-#define JOYSTICK_BUTTON_PIN A2
-#define JOYSTICK_DEADZONE_MIN 256
-#define JOYSTICK_DEADZONE_MAX 768
-#define JOYSTICK_NEUTRAL 512
+#define JOYSTICK_X_PIN PA0
+#define JOYSTICK_Y_PIN PA1
+#define JOYSTICK_BUTTON_PIN PA2
+#define JOYSTICK_DEADZONE_MIN 1024
+#define JOYSTICK_DEADZONE_MAX 3072
+#define JOYSTICK_NEUTRAL 2048
 
 enum Direction
 {
@@ -38,7 +38,7 @@ struct Bullet
   bool active;
 };
 
-const byte TANK_SPRITE[] = {
+const byte TANK_SPRITE[] PROGMEM  = {
   B00001000,
   B11001011,
   B01001010,
@@ -48,7 +48,7 @@ const byte TANK_SPRITE[] = {
   B01111110,
   B11011011,
 };
-const byte TANK_SPRITE_LEFT[] = {
+const byte TANK_SPRITE_RIGHT[] PROGMEM  = {
   B10101010,
   B11111110,
   B01111000,
@@ -58,7 +58,7 @@ const byte TANK_SPRITE_LEFT[] = {
   B11111110,
   B10101010,
 };
-const byte TANK_SPRITE_DOWN[] = {
+const byte TANK_SPRITE_DOWN[] PROGMEM  = {
   B11011011,
   B01111110,
   B11111111,
@@ -68,7 +68,7 @@ const byte TANK_SPRITE_DOWN[] = {
   B11010011,
   B00010000,
 };
-const byte TANK_SPRITE_RIGHT[] = {
+const byte TANK_SPRITE_LEFT[] PROGMEM  = {
   B01010101,
   B01111111,
   B00011110,
@@ -78,7 +78,7 @@ const byte TANK_SPRITE_RIGHT[] = {
   B01111111,
   B01010101,
 };
-const byte BRICK_SPRITE[] = {
+const byte BRICK_SPRITE[] PROGMEM  = {
   B10111011,
   B10111011,
   B00000000,
@@ -88,7 +88,7 @@ const byte BRICK_SPRITE[] = {
   B10111011,
   B10111011,
 };
-const byte HQ_SPRITE[] = {
+const byte HQ_SPRITE[] PROGMEM  = {
   B11011011,
   B01010010,
   B01111110,
@@ -98,7 +98,7 @@ const byte HQ_SPRITE[] = {
   B00111100,
   B01011010,
 };
-const byte ARMOUR_SPRITE[] = {
+const byte ARMOUR_SPRITE[] PROGMEM  = {
   B11101110,
   B11101110,
   B11101110,
@@ -108,6 +108,38 @@ const byte ARMOUR_SPRITE[] = {
   B11101110,
   B00000000,
 };
+const byte WATER_SPRITE[] PROGMEM = {
+  B11111110,
+  B10111111,
+  B11011111,
+  B11101101,
+  B11111110,
+  B11101111,
+  B11010111,
+  B01111111,
+};
+const byte TREE_SPRITE[] PROGMEM = {
+  B00000010,
+  B00010101,
+  B00000111,
+  B00011001,
+  B00101110,
+  B00011111,
+  B11111011,
+  B01101110,
+};
+const uint16_t BRICK_FRONT_COLOR = 0x9A40; //0x9c4a00
+const uint16_t BRICK_BACK_COLOR = 0x630C; //0x636363
+const uint16_t P1_TANK_COLOR = 0xE4E4; //0xe79c21
+const uint16_t P2_TANK_COLOR = 0x0466; //0x008c31
+const uint16_t HQ_COLOR = 0x630C; //0x636363
+const uint16_t ARMOUR_FRONT_COLOR = 0xFFFF; //0xFFFFFF
+const uint16_t ARMOUR_BACK_COLOR = 0x630C; //0x636363
+const uint16_t BULLET_COLOR = 0xAD75; //0xadadad
+const uint16_t WATER_FRONT_COLOR = 0x421F; //0x4242ff
+const uint16_t WATER_BACK_COLOR = 0xB77D; //0xb5efef
+const uint16_t TREE_FRONT_COLOR = 0x8EA0; //0x8cd600
+const uint16_t TREE_BACK_COLOR = 0x0281; //0x005208
 
 enum TileType
 {
@@ -115,43 +147,59 @@ enum TileType
   BRICK = 1,
   HQ = 2,
   ARMOUR = 3,
-  FOREST = 4,//TODO
-  WATER = 5,//TODO
+  TREE = 4,
+  WATER = 5,
 };
 const byte LEVEL1[] PROGMEM = {
-  0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
-  0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1,
-  0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
-  0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1,
-  0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 1, 0, 1, 0, 4, 5, 4, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 5, 5, 5, 0, 1, 0, 1, 0, 1, 0,
+  0, 0, 0, 0, 0, 0, 4, 5, 4, 0, 0, 0, 0, 0, 0, 0,
+  3, 3, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 3,
+  0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 1, 3, 1, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+  0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  3, 3, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 3,
+  0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+  1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,
+  0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0,
 };
 unsigned long lastStepTime = 0;
 unsigned long lastBulletsTime = 0;
 unsigned long lastTempUpdateTime = 0;
-Tank tankPos = {32, 56, DIR_UP, 0};
+Tank tankPos = {40, 152, DIR_UP, 0};
 Bullet bulletPos = {0, 0, DIR_UP, false};
 
-#include <U8g2lib.h>
-U8G2_ST7920_128X64_F_HW_SPI u8g2(U8G2_R0, 10);
+#include <Adafruit_GFX.h> // core graphics library
+#include <Adafruit_ST7735.h> // hardware-specific library
+Adafruit_ST7735 tft(PB5, PB3, PB4);
 
 void clearScreen()
 {
-  u8g2.clearBuffer();
+  tft.fillScreen(ST7735_BLACK);
 }
 
-void drawSprite(byte x, byte y, const byte* sprite)
+void drawSprite(byte x, byte y, const byte* sprite, uint16_t frontColor, uint16_t backColor)
 {
-  u8g2.setColorIndex(1);
-  u8g2.drawXBM(x, y, SPRITE_SIZE, SPRITE_SIZE, sprite);
+  tft.drawBitmap(x, y, sprite, SPRITE_SIZE, SPRITE_SIZE, frontColor, backColor);
+}
+
+void drawTransparentSprite(byte x, byte y, const byte* sprite, uint16_t color)
+{
+  tft.drawBitmap(x, y, sprite, SPRITE_SIZE, SPRITE_SIZE, color);
 }
 
 void clearSprite(byte x, byte y)
 {
-  u8g2.setColorIndex(0);
-  u8g2.drawBox(x, y, SPRITE_SIZE, SPRITE_SIZE);
+  tft.fillRect(x, y, SPRITE_SIZE, SPRITE_SIZE, ST7735_BLACK);
 }
 
 const byte* getSprite(byte type)
@@ -164,8 +212,48 @@ const byte* getSprite(byte type)
       return HQ_SPRITE;
     case ARMOUR:
       return ARMOUR_SPRITE;
+    case TREE:
+      return TREE_SPRITE;
+    case WATER:
+      return WATER_SPRITE;
     default:
       return NULL;
+  };
+}
+
+const uint16_t getFrontColor(byte type)
+{
+  switch (type)
+  {
+    case BRICK:
+      return BRICK_FRONT_COLOR;
+    case HQ:
+      return HQ_COLOR;
+    case ARMOUR:
+      return ARMOUR_FRONT_COLOR;
+    case TREE:
+      return TREE_FRONT_COLOR;
+    case WATER:
+      return WATER_FRONT_COLOR;
+    default:
+      return ST7735_BLACK;
+  };
+}
+
+const uint16_t getBackColor(byte type)
+{
+  switch (type)
+  {
+    case BRICK:
+      return BRICK_BACK_COLOR;
+    case ARMOUR:
+      return ARMOUR_BACK_COLOR;
+    case TREE:
+      return TREE_BACK_COLOR;
+    case WATER:
+      return WATER_BACK_COLOR;
+    default:
+      return ST7735_BLACK;
   };
 }
 
@@ -269,7 +357,7 @@ void updateTankPos(Tank& tankPos)
   if (prevPos.x != tankPos.x || prevPos.y != tankPos.y || prevPos.dir != tankPos.dir)
   {
     clearSprite(prevPos.x, prevPos.y);
-    drawSprite(tankPos.x, tankPos.y, getTankSprite(tankPos.dir));
+    drawTransparentSprite(tankPos.x, tankPos.y, getTankSprite(tankPos.dir), P1_TANK_COLOR);
   }
 }
 
@@ -291,8 +379,7 @@ void updateTankFire(Tank& tankPos)
 
 void drawBullet(byte x, byte y)
 {
-  u8g2.setColorIndex(1);
-  u8g2.drawBox(x - BULLET_SIZE / 2, y - BULLET_SIZE / 2, BULLET_SIZE, BULLET_SIZE);
+  tft.fillRect(x - BULLET_SIZE / 2, y - BULLET_SIZE / 2, BULLET_SIZE, BULLET_SIZE, BULLET_COLOR);
 }
 
 bool updateBullet(Bullet& bullet)
@@ -331,8 +418,7 @@ bool updateBullets()
   if (bulletPos.active)
   {
     // remove bullet at previous pos
-    u8g2.setColorIndex(0);
-    u8g2.drawBox(bulletPos.x - BULLET_SIZE / 2, bulletPos.y - BULLET_SIZE / 2, BULLET_SIZE, BULLET_SIZE);
+    tft.fillRect(bulletPos.x - BULLET_SIZE / 2, bulletPos.y - BULLET_SIZE / 2, BULLET_SIZE, BULLET_SIZE, ST7735_BLACK);
     if (updateBullet(bulletPos))
     {
       drawBullet(bulletPos.x, bulletPos.y);
@@ -343,14 +429,23 @@ bool updateBullets()
 }
 
 #include <OneWire.h>
-OneWire ds(A5);
+#include <Adafruit_BMP085_U.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+OneWire ds(PA3);
+Adafruit_BMP085_Unified  bmp;
+RF24 radio(PB0, PA4); // CE, CSN
+
 struct MeteoState
 {
   float temperature;
   float humidity;
-  unsigned short pressure;
+  unsigned int pressure;
 };
-#define METEO_STATE_DATA_SIZE 10
+#define METEO_STATE_DATA_SIZE 12
+const byte address[6] = "12345";
+
 unsigned int updateTemp()
 {
   byte data[2];
@@ -370,11 +465,10 @@ unsigned int updateTemp()
 
     MeteoState state = {0.0f, 0.0f, 0};
     state.temperature = ((data[1] << 8) | data[0]) * 0.0625;
-    byte* data = (byte*)&state;
-    for (unsigned int i = 0; i < METEO_STATE_DATA_SIZE; ++i)
-    {
-      Serial.write(data[i]);
-    }
+    float P = 0;
+    bmp.getPressure(&P);
+    state.pressure = (unsigned int)P;
+    radio.write(&state, METEO_STATE_DATA_SIZE);
   }
 }
 
@@ -383,22 +477,29 @@ void setup() {
   pinMode(JOYSTICK_BUTTON_PIN, INPUT);
   digitalWrite(JOYSTICK_BUTTON_PIN, HIGH);
   Serial.begin(115200);
-  u8g2.begin();
+  tft.initR (INITR_BLACKTAB);
   clearScreen();
   int idx = 0;
   for (int y = 0; y < TILE_COLUMNS; ++y)
   {
     for (int x = 0; x < TILE_ROWS; ++x)
     {
-      const byte* sprite = getSprite(pgm_read_byte_near(LEVEL1 + idx));
+      byte type = pgm_read_byte_near(LEVEL1 + idx);
+      const byte* sprite = getSprite(type);
       if (sprite)
       {
-        drawSprite(x * SPRITE_SIZE, y * SPRITE_SIZE, sprite);
+        drawSprite(x * SPRITE_SIZE, y * SPRITE_SIZE, sprite, getFrontColor(type), getBackColor(type));
       }
       ++idx;
     }
   }
-  drawSprite(tankPos.x, tankPos.y, getTankSprite(tankPos.dir));
+  drawTransparentSprite(tankPos.x, tankPos.y, getTankSprite(tankPos.dir), P1_TANK_COLOR);
+
+  bmp.begin();
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
 
 void loop() {
@@ -409,15 +510,11 @@ void loop() {
     lastStepTime = timePassed;
     updateTankPos(tankPos);
     updateTankFire(tankPos);
-    u8g2.sendBuffer();
   }
   if (timePassed >= lastBulletsTime + BULLET_STEP_DURATION)
   {
     lastBulletsTime = timePassed;
-    if (updateBullets())
-    {
-      u8g2.sendBuffer();
-    }
+    updateBullets();
   }
   updateTemp();
 }
