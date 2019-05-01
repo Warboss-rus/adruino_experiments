de671bd86c25564352c0dabc6e93db524b2fe0cd
Warboss-rus <Urgaksh@rambler.ru>
2019-05-01 21:20:41 +0300

    [arduino_nrf24l01_serial] Added 128*64 display to the sketch, removed stm32_meteo

--- arduino_nrf24l01_serial.ino
+++ arduino_nrf24l01_serial.ino
@@ -1,17 +1,42 @@
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <U8g2lib.h>
+U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, 6, 3, 5);
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "12345";
+String lastMessage;
 
 struct MeteoState
 {
   float temperature;
   float humidity;
-  unsigned long pressure;
+  uint32_t pressure;
 };
 #define METEO_STATE_DATA_SIZE 12
+
+void printState(const MeteoState& state)
+{
+  String tempString = "Temp: ";
+  tempString += String(state.temperature);
+  tempString += "°C";
+  String pressureString = "Pressure: ";
+  pressureString += String(state.pressure);
+  pressureString += " Pa";
+  String humidityString = "Humidity: ";
+  humidityString += state.humidity;
+  humidityString += "%";
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    u8g2.drawStr(0, 11, tempString.c_str());
+    u8g2.drawStr(0, 28, pressureString.c_str());
+    u8g2.drawStr(0, 45, humidityString.c_str());
+    u8g2.drawStr(0, 62, lastMessage.c_str());
+    u8g2.sendBuffer();
+  } while ( u8g2.nextPage() );
+}
 
 void setup()
 {
@@ -21,12 +46,19 @@ void setup()
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+  u8g2.begin();
+  printState(MeteoState{0.f, 0.f, 0});
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  if (radio.available()) {
+  if (Serial.available()) 
+  {
+    lastMessage = Serial.readString();
+  }
+  if (radio.available()) 
+  {
     MeteoState state;
     radio.read(&state, METEO_STATE_DATA_SIZE);
     const byte* data = (const byte*)&state;
@@ -34,5 +66,6 @@ void loop()
     {
       Serial.write(data[i]);
     }
+    printState(state);
   }
 }
